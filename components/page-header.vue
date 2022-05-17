70cf8af5dafd95af943bbc7b37336b30ad835a34
Igor Trindade <igortrindademe@gmail.com>
2022-05-17 14:54:52 -0300

    Updated logo filename

--- components/page-header.vue
+++ components/page-header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="fixed top-0 right-0 left-0 z-50 w-full flex flex-wrap justify-center items-center h-24 font-sans bg-gradient-to-br from-cyan-900 to-cyan-800 shadow-xl">
     <div class="w-full px-6 md:px-20 xl:px-28 flex">
-      <img src="/images/logo/1x/LOGO_2 1_1x.png" class="h-14" alt="Logo Calculo e previdência"/>
+      <img src="/images/logo/1x/LOGO_2_1_1x.png" class="h-14" alt="Logo Calculo e previdência"/>
     </div>
   </nav>
 </template>
