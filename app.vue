f697e6df7416e8ccbe40169296c023ed348a468d
Igor Trindade <igortrindademe@gmail.com>
2022-05-17 14:51:47 -0300

    Added logos

--- app.vue
+++ app.vue
@@ -3,8 +3,9 @@
   <Head>
     <Title>Cálculo e Previdência</Title>
     <Meta name="description" content="Titulo" />
+    <Link rel="icon" href="/images/logo/128w/ICON_2_128w.png" />
   </Head>
-  
+
   <div class="w-screen h-screen font-sans bg-white text-zinc-700 overflow-y-auto p-12 pt-32 px-6 md:px-20 xl:px-28 scrolling-touch" style="-webkit-overflow-scrolling: touch;">
     <NuxtLayout>
       <NuxtPage/>
