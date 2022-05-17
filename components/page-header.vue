f6fb000c0b5d016720a61a16f69ba1e1bb654b4a
Igor Trindade <igortrindademe@gmail.com>
2022-05-17 14:10:52 -0300

    Changes images from assets to public folder

--- components/page-header.vue
+++ components/page-header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="fixed top-0 right-0 left-0 z-50 w-full flex flex-wrap justify-center items-center h-24 font-sans bg-gradient-to-br from-cyan-900 to-cyan-800 shadow-xl">
     <div class="w-full px-6 md:px-20 xl:px-28 flex">
-      <img src="/assets/images/LOGO_1.png" class="h-14" />
+      <img src="/images/LOGO_1.png" class="h-14" />
     </div>
   </nav>
 </template>
