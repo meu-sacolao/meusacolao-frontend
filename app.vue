e8e256052f70ae3a700470716dbe313a8d9bb454
Igor Trindade <igortrindademe@gmail.com>
2022-05-18 17:05:54 -0300

    Fix on html attrs and meta

--- app.vue
+++ app.vue
@@ -1,18 +1,24 @@
 <template>
 
-  <Head>
-    <Title>Cálculo e Previdência</Title>
-    <Meta name="description" content="Titulo" />
-    <Link rel="icon" href="/images/logo/128w/ICON_2_128w.png" />
-  </Head>
-
-  <div class="w-screen h-screen font-sans bg-white text-zinc-700 overflow-y-auto p-12 pt-32 px-6 md:px-20 xl:px-28 scrolling-touch" style="-webkit-overflow-scrolling: touch;">
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </div>
+  <Html lang="pt-br">
+    <Head>
+      <Title>Cálculo e Previdência</Title>
+      <Meta name="description" content="Titulo" />
+      <Meta charset="utf-8" />
+      <Meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <Meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      <Link rel="icon" href="/images/logo/128w/ICON_2_128w.png" />
+    </Head>
+    <Body>
+      <div class="w-screen h-screen font-sans bg-white text-zinc-700 overflow-y-auto p-12 pt-32 px-6 md:px-20 xl:px-28 scrolling-touch" style="-webkit-overflow-scrolling: touch;">
+        <NuxtLayout>
+          <NuxtPage/>
+        </NuxtLayout>
+      </div>
+    </Body>
+  </Html>
 </template>
 
 <script setup>
-  import '@/assets/main.css'
+  import '@/assets/css/main.scss'
 </script>
