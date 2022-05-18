560163c36b06f6de08e888e377cd3bbfc80c486b
Igor Trindade <igortrindademe@gmail.com>
2022-05-18 17:05:41 -0300

    Card

---
+++ components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-full block shadow-lg bg-white border border-slate-100 flex">
+
+    <div :class="border" class="h-full block"></div>
+    <div :class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    border: {
+      type: String,
+      default: 'w-3 bg-orange-400'
+    },
+    content: {
+      type: String,
+      default: 'p-8'
+    }
+  }
+}
+</script>
