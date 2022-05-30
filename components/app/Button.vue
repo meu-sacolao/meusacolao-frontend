f1dc7135f6b55a04e637f72b45076078df46aa4d
Igor Trindade <igortrindademe@gmail.com>
2022-05-30 14:13:50 -0300

    Button component

---
+++ components/app/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <button 
+    class="px-3 py-2 flex items-center justify-center"
+    :class="[bg, text]"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: 'Button',
+  props: ['bg', 'text', 'disabled']
+}
+</script>
