af277e243c6da92c7e64a9552f41308c190706ea
Igor Trindade <igortrindademe@gmail.com>
2022-06-11 17:14:31 -0300

    Aside menu

---
+++ components/app/Aside.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <div 
+      class="mt-24 fixed top-0 right-0 w-full translate-x-full lg:max-w-lg p-12 transition-all duration-300 easy-in-out h-screen bg-brand-grandient overflow-hidden flex items-start justify-center"
+      :class="[menuIsOpen ? '-translate-x-0' : '']"
+    >
+
+    asdasd
+
+      <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Aside',
+  props: ['menuIsOpen'],
+  expose: ['toggleDrawer'],
+  emits: ['update:menuIsOpen'],
+  mounted() {
+    document.addEventListener("keyup", this.handleEsc)
+  },
+  beforeDestroy() {
+    document.removeEventListener("keyup", this.handleEsc)
+  },
+  methods: {
+    handleEsc(evt) {
+      if (this.menuIsOpen && evt.keyCode === 27) this.toggleDrawer()
+    },
+    toggleDrawer() {
+      console.log('Event chegou aqui')
+      this.$emit('update:menuIsOpen', !this.menuIsOpen)
+    }
+  }
+}
+</script>
--- components/app/PageHeader.vue
+++ components/app/PageHeader.vue
@@ -1,14 +1,39 @@
 <template>
-  <nav class="fixed top-0 right-0 left-0 z-50 w-full flex flex-wrap justify-center items-center h-24 font-sans bg-gradient-to-br from-cyan-900 to-cyan-800 shadow-xl">
-    <div class="w-full px-6 md:px-20 xl:px-28 flex">
+  <nav class="fixed top-0 right-0 left-0 z-50 w-full px-6 md:px-20 xl:px-28 flex flex-wrap justify-between items-center h-24 font-sans bg-gradient-to-br from-cyan-900 to-cyan-800 shadow-xl">
+    <div class=" flex">
       <img src="/images/logo/1x/LOGO_2_1_1x.png" class="h-14" alt="Logo Calculo e previdência"/>
     </div>
+
+    <div class="flex">
+      </div>
+    <button 
+      @click="toggleMenu()"
+      class="menu-toggle" 
+      :class="{ 'active' : menuIsOpen }"
+    >
+      <span class="menu-toggle-bar"></span>
+      <span class="menu-toggle-bar"></span>
+      <span class="menu-toggle-bar"></span>
+    </button>
+
+    <AppAside ref="AppAside"  v-model:menuIsOpen="menuIsOpen" />
   </nav>
 </template>
 
 <script>
 export default {
-
+  name: 'AppPageHeader',
+  data() {
+    return {
+      menuIsOpen: true
+    }
+  },
+  methods: {
+    toggleMenu() {
+      console.log(this.$refs['AppAside'])
+      this.$refs['AppAside'].toggleDrawer()
+    }
+  }
 }
 </script>
 
