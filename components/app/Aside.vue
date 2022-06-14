a2682f68331ea9e2fd0a353c350bfce4024d1d3a
Igor Trindade <igortrindademe@gmail.com>
2022-06-13 21:12:03 -0300

    General improvements

--- components/app/Aside.vue
+++ components/app/Aside.vue
@@ -1,14 +1,13 @@
 <template>
-
-    <div 
-      class="mt-24 fixed top-0 right-0 w-full translate-x-full lg:max-w-lg p-12 transition-all duration-300 easy-in-out h-screen bg-brand-grandient overflow-hidden flex items-start justify-center"
-      :class="[menuIsOpen ? '-translate-x-0' : '']"
-    >
-
-    asdasd
-
-      <slot></slot>
-    </div>
+  <aside 
+    ref="aside"
+    class="mt-24 fixed top-0 right-0 w-full translate-x-full lg:max-w-lg p-12 transition-all duration-300 easy-in-out h-screen bg-brand-grandient overflow-hidden flex items-start justify-center"
+    :class="[menuIsOpen ? '-translate-x-0' : '']"
+  >
+    <AppButton @click.prevent="$eventBus.$emit('openModal')" bg="bg-slate-700" text="text-white">
+      Login
+    </AppButton>
+  </aside>
 </template>
 
 <script>
@@ -17,20 +16,37 @@ export default {
   props: ['menuIsOpen'],
   expose: ['toggleDrawer'],
   emits: ['update:menuIsOpen'],
-  mounted() {
-    document.addEventListener("keyup", this.handleEsc)
-  },
-  beforeDestroy() {
-    document.removeEventListener("keyup", this.handleEsc)
-  },
   methods: {
+
+    toggleDrawer() {
+      if(!this.menuIsOpen) {
+        this.addListeners()
+      } else {
+        this.removeListeners()
+      }
+      this.$emit('update:menuIsOpen', !this.menuIsOpen)
+    },
+
+    addListeners() {
+      document.addEventListener("click", this.handleClickOutside)
+      document.addEventListener("keyup", this.handleEsc)
+    },
+
+    removeListeners() {
+      document.removeEventListener("click", this.handleClickOutside)
+      document.removeEventListener("keyup", this.handleEsc)
+    },
+
     handleEsc(evt) {
       if (this.menuIsOpen && evt.keyCode === 27) this.toggleDrawer()
     },
-    toggleDrawer() {
-      console.log('Event chegou aqui')
-      this.$emit('update:menuIsOpen', !this.menuIsOpen)
+
+    handleClickOutside(event) {
+      if (!(this.$refs['aside'] == event.target || this.$refs['aside'].contains(event.target))) {
+        this.toggleDrawer()
+      }
     }
+    
   }
 }
 </script>
--- entitties/User.ts
+++
@@ -1,9 +0,0 @@
-export default class User {
-  nit: string = ''
-  cpf: string = ''
-  name: string = ''
-  gender: string = ''
-  birthDate: string = ''
-  motherName: string = ''
-  email: string = ''
-}
