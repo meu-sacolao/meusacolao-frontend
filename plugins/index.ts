1aad41850962fffc1496b9e4882a6417d12a1440
Igor Trindade <igortrindademe@gmail.com>
2022-06-13 21:35:02 -0300

    Revert "Removed globals"
    This reverts commit 50038fa05ac8c0024d18dcd976e9e66cddf30df3.

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,10 +1,10 @@
 
 // import setupDirectives from '@/setup/directives'
-// import setupComponents from '@/setup/components'
-// import setupGlobals from '@/setup/globals'
+import setupComponents from '@/setup/components'
+import setupGlobals from '@/setup/globals'
 
 export default defineNuxtPlugin((nuxtApp) => {
   // setupDirectives(nuxtApp.vueApp)
-  // setupComponents(nuxtApp.vueApp)
-  // setupGlobals(nuxtApp.vueApp)
+  setupComponents(nuxtApp.vueApp)
+  setupGlobals(nuxtApp.vueApp)
 })
