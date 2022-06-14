e34bc809a8c0906aeb89fb5395fece15a54155b0
Igor Trindade <igortrindademe@gmail.com>
2022-06-13 21:35:07 -0300

    Revert "Removed directives"
    This reverts commit 77733617cb22ac1580a2dfa895f264bc21403316.

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,10 +1,10 @@
 
-// import setupDirectives from '@/setup/directives'
+import setupDirectives from '@/setup/directives'
 import setupComponents from '@/setup/components'
 import setupGlobals from '@/setup/globals'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // setupDirectives(nuxtApp.vueApp)
+  setupDirectives(nuxtApp.vueApp)
   setupComponents(nuxtApp.vueApp)
   setupGlobals(nuxtApp.vueApp)
 })
