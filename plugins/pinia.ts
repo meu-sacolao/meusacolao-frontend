987f836ab8e2abf133210a653598aef983ae284a
Igor Trindade <igortrindademe@gmail.com>
2022-06-15 18:43:28 -0300

    Removed access on Nuxt #app

--- plugins/pinia.ts
+++ plugins/pinia.ts
@@ -1,5 +1,4 @@
 import { createNuxtPersistedState } from 'pinia-plugin-persistedstate'
-import { defineNuxtPlugin, useCookie } from '#app'
 
 export default defineNuxtPlugin(nuxtApp => {
   nuxtApp.$pinia.use(createNuxtPersistedState(useCookie))
