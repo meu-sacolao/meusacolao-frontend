11534cee52ad24b7465bb081f98f636dd7205b13
Igor Trindade <igortrindademe@gmail.com>
2022-06-26 19:47:38 -0300

    middleware auth-admin fix

--- middleware/auth-admin.ts
+++ middleware/auth-admin.ts
@@ -3,5 +3,5 @@ import { useAuthStore } from "@/modules/auth/store"
 export default defineNuxtRouteMiddleware((to, from) => {
   const authStore = useAuthStore()
   const { loggedUser } = authStore
-  if(loggedUser.role !== 'ADMIN') return navigateTo('/')
+  if(!loggedUser || loggedUser.role !== 'ADMIN') return navigateTo('/')
 })
