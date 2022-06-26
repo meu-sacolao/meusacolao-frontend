ef6cec535a5fbbcfd1230596d31a9aac0f8e95d0
Igor Trindade <igortrindademe@gmail.com>
2022-06-26 19:47:47 -0300

    Emits close for baseModal

--- modules/auth/AuthForm.vue
+++ modules/auth/AuthForm.vue
@@ -5,18 +5,31 @@
     <AppInputWithIcon v-model:value="user.email" icon="email" type="email" label="Email" placeholder="Insira seu email" />
     <AppInputWithIcon v-model:value="user.password" icon="key" type="password" label="Senha" placeholder="Insira sua senha" />
 
-    <AppButton @click="authStore.login({ email: user.email, password: user.password })">
+    <AppButton @click="login()">
       Entrar
     </AppButton>
   </div>
 </template>
 
 <script setup>
-import { useAuthStore } from "@/modules/auth/store"
-import User from '@/entities/User'
+  import { defineEmits, getCurrentInstance } from 'vue'
+  import { useAuthStore } from "@/modules/auth/store"
+  import User from '@/entities/User'
 
-const authStore = useAuthStore()
+  const authStore = useAuthStore()
+  const { emit } = getCurrentInstance()
+  defineEmits(['close'])
 
-const user = ref(new User())
+  const user = ref(new User())
+
+  const login = () => {
+    authStore.login({ email: user.value.email, password: user.value.password })
+      .then(() => {
+        emit('close')
+      })
+      .catch(() => {
+        alert('Erro ao logar')
+      })
+  }
 
 </script>
