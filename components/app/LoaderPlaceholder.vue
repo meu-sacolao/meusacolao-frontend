dde5217ccd20b17370ed949e462f63c3a20e3550
Igor Trindade <igortrindademe@gmail.com>
2022-07-02 08:23:10 -0300

    LoaderPlaceHolder

---
+++ components/app/LoaderPlaceholder.vue
@@ -0,0 +1,24 @@
+<template>
+  <ClientOnly>
+    <div class="w-full flex flex-col">
+      <div 
+        class="w-full bg-zinc-200"
+        :class="classes"
+        v-for="(i, index) in quantity"
+        :key="`loader-placeholder-${index}`"
+      >
+      </div>
+    </div>
+  </ClientOnly>
+</template>
+
+<script setup>
+  defineProps({
+    quantity: {
+      default: 1
+    },
+    classes: {
+      default: 'h-32'
+    }
+  })
+</script>
