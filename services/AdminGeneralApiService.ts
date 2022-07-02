fc392baf31fc84373fbdb5899546f662ec468085
Igor Trindade <igortrindademe@gmail.com>
2022-07-02 08:23:38 -0300

    General file upload fix

--- services/AdminGeneralApiService.ts
+++ services/AdminGeneralApiService.ts
@@ -1,9 +1,26 @@
 import Api from '@/util/Api'
-
+import FileInfo from '@/entities/FileInfo'
 export default class AdminGeneralApiService {
 
   static updateOrCreate(entity, instance) {
-    return Api.post(`/admin/general/updateOrCreate`, { entity, ...instance })
+
+    if(instance.file && instance.file.name) {
+
+      const fd = new FormData()
+      fd.append('entity', entity)
+
+      Object.keys(instance).forEach((key) => {
+        if( ![undefined, null].includes(instance[key]) )  {
+          fd.append(key, instance[key])
+        }
+      })
+
+      return Api.post(`/admin/general/updateOrCreate`, fd, { headers: { 'Content-Type': 'multipart/form-data' } })
+
+    } else {
+      return Api.post(`/admin/general/updateOrCreate`, { entity, ...instance })
+    }
+
   }
 
 
