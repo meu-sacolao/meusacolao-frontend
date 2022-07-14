f2c71ecf75082467c4b38a18a9acdcd01a34f31e
Igor Trindade <igortrindademe@gmail.com>
2022-07-14 13:14:59 -0300

    Testing general error handler condition

--- setup/generalErrorHandler.ts
+++ setup/generalErrorHandler.ts
@@ -1,16 +1,19 @@
 
 
 export default () => {
-  const messages = [
-    `Uncaught NotFoundError: Failed to execute 'insertBefore' on 'Node': The node before which the new node is to be inserted is not a child of this node.`, // chromium based
-    `NotFoundError: The object can not be found here.`, // safari
-  ]
-  if (typeof window !== 'undefined') {
-    window.addEventListener('error', (ev) => {
-      if (messages.includes(ev.message)) {
-        ev.preventDefault()
-        window.location.reload()
-      }
-    })
+
+  if(process.client) {
+    const messages = [
+      `Uncaught NotFoundError: Failed to execute 'insertBefore' on 'Node': The node before which the new node is to be inserted is not a child of this node.`, // chromium based
+      `NotFoundError: The object can not be found here.`, // safari
+    ]
+    if (typeof window !== 'undefined') {
+      window.addEventListener('error', (ev) => {
+        if (messages.includes(ev.message)) {
+          ev.preventDefault()
+          window.location.reload()
+        }
+      })
+    }
   }
 }
