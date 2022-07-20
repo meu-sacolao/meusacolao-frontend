85ef9a44b2c2ce3460dac394abfe62c6cd43d1b6
Igor Trindade <igortrindademe@gmail.com>
2022-07-19 21:10:12 -0300

    Separando regras da simulação em componente especifico

--- modules/app/simulation/result/ResultRetirementGroupCard.vue
+++ modules/app/simulation/result/ResultRetirementGroupCard.vue
@@ -11,62 +11,11 @@
     </template>
     <template v-slot:content>
 
-      <div class="w-full flex flex-col">
-        <div 
-          class="w-full flex flex-col shadow mb-4 p-4 relative" 
+        <ResultRetirementOptionCard
           v-for="(simulationRetirementOption, index) in simulationRetirementGroup.simulationRetirementOptions" 
+          :simulationRetirementOption="simulationRetirementOption"
           :key="`simulationRetirementOption${index}`"
-        >
-
-          <div class="w-full flex flex-wrap mb-4">
-            <div class="mr-4">
-              <AppPill v-if="simulationRetirementOption.isGranted" bg="bg-emerald-600" text="text-white">
-                <AppIcons icon="check_circle" />
-                <span class="ml-2">Direito ao Benefício</span>
-              </AppPill>
-              <AppPill v-else bg="bg-zinc-300">
-                <AppIcons icon="block" />
-                <span class="ml-2">Não tem direito ao benefício</span>
-              </AppPill>
-            </div>
-            <h3 class="h4 leading-relaxed">{{ simulationRetirementOption.retirementOption.title }}</h3>
-          </div>
-
-          <div class="p text-slate-400 leading-relaxed" v-html="simulationRetirementOption.retirementOption.content"></div>
-
-          <div class="w-full flex flex-wrap">
-            <AppLabelValue v-if="simulationRetirementOption.age" class="four-cols-breakdown">
-              <template v-slot:label>Idade</template>
-              <template v-slot:value>{{ simulationRetirementOption.age.time.timeText }}</template>
-            </AppLabelValue>
-            <AppLabelValue v-if="simulationRetirementOption.contributionTime" class="four-cols-breakdown">
-              <template v-slot:label>Tempo de contribuição</template>
-              <template v-slot:value>{{ simulationRetirementOption.contributionTime.time.timeText }}</template>
-            </AppLabelValue>
-          </div>
-          <!-- REQUIREMENTS RESULT -->
-          <div class="w-full flex flex-col mt-4">
-            <div 
-              class="w-full flex items-center"
-              v-for="(requirement, index) in simulationRetirementOption.requirements"
-              :key="`retirementOptionError${index}`"
-            >
-              <AppIcons :icon="requirement.isGranted ? 'check' : 'warning'" :class="[!requirement.isGranted ? 'text-red-400' : 'text-green-600']" />
-              <p class="ml-2 p truncate text-slate-600 leading-relaxed ...">{{ requirement.content }}</p>
-            </div>
-          </div>
-
-          <div class="flex mt-6">
-            <AppButton 
-              bg="bg-slate-300" 
-              @click="emitter.emit('openDrawer', { component: 'SimulationRetirementOptionDetailDrawer', payload: simulationRetirementOption })">
-                Ver cálculo
-            </AppButton>
-          </div>
-          
-        </div>
-
-      </div>
+        />
 
       <p class="text-lg italic" v-if="isGranted">
         Pelos elementos apresentados, você tem direito à aposentadoria pela regra <b>{{ simulationRetirementGroup.retirementGroup.title }}</b>.
@@ -84,17 +33,14 @@
 
 <script setup>
   
+  import ResultRetirementOptionCard from '@/modules/app/simulation/result/ResultRetirementOptionCard'
+
   const props = defineProps({
     simulationRetirementGroup: Object
   })
-
-  const showContent = ref(false)
 
   const isGranted = computed(() => {
     return props.simulationRetirementGroup.simulationRetirementOptions.filter((i) => i.isGranted).length > 0
   })
 
-  const toggleCard = () => {
-    showContent.value = !showContent.value
-  }
 </script>
---
+++ modules/app/simulation/result/ResultRetirementOptionCard.vue
@@ -0,0 +1,59 @@
+<template>
+
+
+  <div class="w-full flex flex-col space-y-4 shadow hover:shadow-md border border-zinc-100 mb-4 p-4 relative">
+    <div class="w-full flex flex-wrap">
+      <div class="mr-4">
+        <AppPill v-if="simulationRetirementOption.isGranted" bg="bg-emerald-600" text="text-white">
+          <AppIcons icon="check_circle" />
+          <span class="ml-2">Direito ao Benefício</span>
+        </AppPill>
+        <AppPill v-else bg="bg-zinc-300">
+          <AppIcons icon="block" />
+          <span class="ml-2">Não tem direito ao benefício</span>
+        </AppPill>
+      </div>
+      <h3 class="h4 leading-relaxed">{{ simulationRetirementOption.retirementOption.title }}</h3>
+    </div>
+
+    <div class="p text-slate-400 leading-relaxed" v-html="simulationRetirementOption.retirementOption.content"></div>
+
+    <div class="w-full flex flex-col space-y-4" v-if="simulationRetirementOption.age || simulationRetirementOption.contributionTime">
+      <AppLabelValue v-if="simulationRetirementOption.age">
+        <template v-slot:label>Idade</template>
+        <template v-slot:value>{{ simulationRetirementOption.age.time.timeText }}</template>
+      </AppLabelValue>
+      <AppLabelValue v-if="simulationRetirementOption.contributionTime">
+        <template v-slot:label>Tempo de contribuição</template>
+        <template v-slot:value>{{ simulationRetirementOption.contributionTime.time.timeText }}</template>
+      </AppLabelValue>
+    </div>
+    <!-- REQUIREMENTS RESULT -->
+    <div class="w-full flex flex-col">
+      <div 
+        class="w-full flex items-center"
+        v-for="(requirement, index) in simulationRetirementOption.requirements"
+        :key="`retirementOptionError${index}`"
+      >
+        <AppIcons :icon="requirement.isGranted ? 'check' : 'warning'" :class="[!requirement.isGranted ? 'text-red-400' : 'text-green-600']" />
+        <p class="ml-2 p truncate text-slate-600 leading-relaxed ...">{{ requirement.content }}</p>
+      </div>
+    </div>
+
+    <div class="flex mt-6">
+      <AppButton 
+        bg="bg-slate-300" 
+        @click="emitter.emit('openDrawer', { component: 'SimulationRetirementOptionDetailDrawer', payload: simulationRetirementOption })">
+          Ver cálculo
+      </AppButton>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+  const props = defineProps({
+    simulationRetirementOption: Object,
+  })
+  
+</script>
