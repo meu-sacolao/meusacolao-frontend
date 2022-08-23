d9236e54be545c1f0779f0a30c12c04b74e6c907
Igor Trindade <igortrindademe@gmail.com>
2022-08-23 10:40:20 -0300

    fix: Added prefetch to illustrations pack

--- app.vue
+++ app.vue
@@ -8,6 +8,15 @@
       <Meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <Meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <Link rel="icon" href="/images/logo/128w/ICON_2_128w.png" />
+
+      <Link 
+        v-for="index in 21" 
+        :key="`pack-image-${index}`" 
+        rel="prefetch" 
+        as="image" 
+        :href="`/images/general/pack-illustrations/${index < 10 ? `0${index}` : index}.svg`" 
+      />
+
     </Head>
     <Body id="body" class="w-screen h-screen font-sans bg-slate-100 text-zinc-700 overflow-x-hidden scrolling-touch relative">
 
@@ -31,7 +40,6 @@
       mode: 'out-in'
     }
   })
-
 
 </script>
 
