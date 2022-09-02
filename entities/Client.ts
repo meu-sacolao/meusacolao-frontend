8486141cb7ad0ea6f2a5af0f3842e44d2339c41f
Igor Trindade <igortrindademe@gmail.com>
2022-09-02 08:45:13 -0300

    chore: Limpado formulatio segurado

--- entities/Client.ts
+++ entities/Client.ts
@@ -3,14 +3,14 @@ import BaseModel from '@/entities/BaseModel'
 export default class Client extends BaseModel {
   id: string | null = null
   key: string | null = null
-  name: string = 'Igor'
-  email: string = 'igorlucast@hotmail.com'
-  phone: string = '(31)99999-9999'
-  birthDate: string = '18/06/1986'
-  motherName: string = 'Regina'
-  cpf: string = '066.124.296-07'
-  nit: string = '12.123123'
-  gender: string = 'Masculino'
+  name: string = ''
+  email: string = ''
+  phone: string = ''
+  birthDate: string = ''
+  motherName: string = ''
+  cpf: string = ''
+  nit: string = ''
+  gender: string = ''
   createdAt: string = ''
   updatedAt: string = ''
   
